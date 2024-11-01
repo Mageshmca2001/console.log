b74f36bf7daee9ea96ed381b31a5aee54d4fda53
Mageshmca2001 <Mageshmca2001@gmail.com>
2024-11-01 14:37:39 +0530

    first draft

---
+++ Magesh.app/Magesh.app/Program.cs
@@ -0,0 +1,191 @@
+﻿/*namespace  Datatype
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            int x = 10;
+            int y = 10;
+            int z = x + y;
+            Console.WriteLine (z);
+        }
+    }
+}*/
+
+
+/*namespace Datatype
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 1.234f;
+            float num2 = 3.206f;
+            float num3 = 4.567f;
+            float num4 = num1+num2+num3;
+            Console.WriteLine(num4);
+        }
+    }
+}*/
+
+/*namespace Magesh
+{
+    class progrma
+    {
+        static void Main(string[] args)
+        {
+            double var = 5.678;
+            double let = 6.789;
+            double var2 = 8.907;
+
+            double boolean = var + var2 +let;
+
+            Console.WriteLine(boolean);
+
+        }
+    }
+}*/
+
+/*namespace jackson
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            long num1 = 30000;
+            long num2 = 40000;
+            long num3 = 60000;
+
+            long num4 = num1+num2+num3;
+
+            Console.WriteLine(num4);
+        }
+    }
+}*/
+
+/*namespace kernal
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            float num = 3.4567f;
+            int a =(int) num;
+            Console.WriteLine(a);
+
+            int b = 10;
+            long num1 = b;
+
+            Console.Write(num1.GetType().Name);
+        }
+    }
+}*/
+
+/*namespace loop
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 30;
+            int c = 40;
+            int z = a + b + c;
+            Console.WriteLine("print value of c " + c);
+        }
+    }
+}*/
+
+/*namespace loin
+{
+   class program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 1.4567f;
+            float num2 = 2.4567f;
+            float num3 = num1 / num2;
+
+            Console.WriteLine(num3);
+        }
+    }
+}*/
+
+/*namespace kernal
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            long b = 100;
+            int num1 = (int)b;
+            Console.WriteLine(num1);
+            Console.WriteLine(num1.GetType().ToString());
+        }
+    }
+}*/
+
+/*namespace Getch
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a two Number");
+
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            int num1 =int.Parse(a);
+            int num2 =int.Parse(b);
+
+            int num3 =num1 + num2;
+
+            Console.WriteLine(num3);
+        }
+    }
+}*/
+
+/*namespace Getch
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string name = " Magesh.B";
+
+
+            int age = 30;
+
+            string location = "57 Vallalar st periyakuppam,tiruvallur";
+
+            Console.WriteLine("Hi My Name is " +name+ "My age is " +age+ "iam from" + location);
+          
+        }
+    }
+}*/
+
+
+
+/*namespace jack
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string Emp_name = "Magesh.B";
+            string Emp_location = "57 Vallalar st periyakuppam Tiruvallur";
+            string Emp_age = "50";
+            string Emp_Hired = "10-12-2026";
+
+
+            Console.WriteLine(Emp_name );
+            Console.WriteLine( Emp_location );
+            Console.WriteLine( Emp_age );
+            Console.WriteLine( Emp_Hired );
+        }
+    }
+}*/
+
+
+
